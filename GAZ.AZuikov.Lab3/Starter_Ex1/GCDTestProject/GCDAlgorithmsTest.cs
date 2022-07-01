fa79da02ebd634f4e61a9b29cd32077e25fa4093
Artyom Zuykov <zujkov.ks52@gmail.com>
2022-07-01 16:53:21 +0300

    third lab is done

---
+++ GAZ.AZuikov.Lab3/Starter_Ex1/GCDTestProject/GCDAlgorithmsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GreatestCommonDivisor;
+
+namespace GCDTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FindGCDEuclidTest()
+        {
+            int a = 2806;
+            int b = 345;
+            int expected = 23;
+
+            int actual = GCDAlgorithms.FindGCDEuclid(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
