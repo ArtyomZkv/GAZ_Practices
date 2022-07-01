888955ef0b97c3e4ded3e4735d9e83cba8dcde13
Artyom Zuykov <zujkov.ks52@gmail.com>
2022-07-01 16:45:37 +0300

    second lab is done

---
+++ GAZ.AZuikov.Lab2/GAZ.AZuikov.Lab2/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace GAZ.AZuikov.Lab2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
